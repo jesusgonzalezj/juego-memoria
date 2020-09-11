046d0298ced10aa3c17759718c39cef61d3fb568
Jesus R. Gonzalez J <JGONZ@MacBook-Pro-JGONZ.local>
2020-09-11 19:43:02 -0300

    Updates

---
+++ precache-manifest.b6fee619e8e9500825c171f3f2d81dbb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488ffae5f550559bc8da0a4bbcffa776",
+    "url": "/juego-memoria/index.html"
+  },
+  {
+    "revision": "74e90f0272eeed6c00bd",
+    "url": "/juego-memoria/static/css/2.030109f1.chunk.css"
+  },
+  {
+    "revision": "eb7d585c35c6ddda5d38",
+    "url": "/juego-memoria/static/css/main.4778f1a7.chunk.css"
+  },
+  {
+    "revision": "74e90f0272eeed6c00bd",
+    "url": "/juego-memoria/static/js/2.dfa4d4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/juego-memoria/static/js/2.dfa4d4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb7d585c35c6ddda5d38",
+    "url": "/juego-memoria/static/js/main.9787482b.chunk.js"
+  },
+  {
+    "revision": "f29f90051355b23c8168",
+    "url": "/juego-memoria/static/js/runtime-main.f7d53771.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/juego-memoria/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/juego-memoria/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/juego-memoria/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/juego-memoria/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/juego-memoria/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
